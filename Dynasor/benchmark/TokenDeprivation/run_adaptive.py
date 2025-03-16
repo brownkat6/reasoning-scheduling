6f45c383aa350d20e288c1d8b08a2970a7929345
Katrina <katrinabrown@college.harvard.edu>
2025-03-16 18:05:52 +0200

    train

--- Dynasor/benchmark/TokenDeprivation/run_adaptive.py
+++ Dynasor/benchmark/TokenDeprivation/run_adaptive.py
@@ -10,6 +10,7 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(f"Adding {os.path.dirname(os.path.dirname(os.path.abspath(__file__)))} to path")
 from mlp_test import MLP
 from run import execute_question_reuse
 import os
