1c8a9fdab2269cbd29bc764c4c4d2f0380cb5c95
Katrina <katrinabrown@college.harvard.edu>
2025-03-24 17:26:13 -0400

    train

--- Dynasor/benchmark/TokenDeprivation/run_adaptive.py
+++ Dynasor/benchmark/TokenDeprivation/run_adaptive.py
@@ -194,6 +194,7 @@ def main():
             mlp_model = checkpoint['model'].cuda()
         else:
             config = checkpoint['config']
+            config['hidden_dim']=256
             mlp_model = MLP(
                 input_dim=config['input_dim'],
                 hidden_dim=config['hidden_dim'],
