e29b210ff79f8673bb2e301a96b368854cd8f908
Katrina <katrinabrown@college.harvard.edu>
2025-03-31 11:34:47 -0400

    train

--- mlp_train.py
+++ mlp_train.py
@@ -74,6 +74,7 @@ def train_mlp(train_data_dir='', train_split='train', train_dataset='gsm8k',
                 on=['question_id', 'dataset', 'split'],
                 how='inner'
             )
+            print(merged_data.columns)
             if 'question_text_x' in merged_data.columns and 'question_text_y' in merged_data.columns:
                 assert (merged_data['question_text_x'] == merged_data['question_text_y']).all()
                 merged_data.drop(columns=['question_text_x', 'question_text_y'], inplace=True)
